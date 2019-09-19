9002e1d082f4b94c832ad813ddf8d6be50c802c4
Rowan Atkinson <rowaneatkinson@gmail.com>
2019-09-19 18:38:31 -0500

    made first draft of note page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Notepad>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Notepad from './components/Notepad.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Notepad
   }
 }
 </script>
---
+++ src/components/Notepad.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Course Name</h1>    
+    <h1>Topic</h1><h1>Date</h1>
+
+    <form>
+      <input type="textarea" col="10" rol="20" value="TYPE NOTES!!">
+    </form>
+  </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
