9cd5b69a46ed52cc7abfb06d825dfabb6307ac53
Zidane <zidane.wright@speurgroup.com>
2019-09-19 18:35:45 -0500

    create sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidebar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from '@/components/Sidebar.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li v-for="note of notes" :key="note">{{ note.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      notes: []
+    }
+  },
+
+  created() {
+    this.notes = [{title: 'test', body: 'testbody'}]
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.sidebar {
+  width: 15%;
+  height: 100vh;
+  background: pink;
+}
+</style>
